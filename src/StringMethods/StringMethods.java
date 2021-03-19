bd8ee3f2c043b5467eb3c56a8979d5d8af42fba3
Abby Beulke <abbybeulke@gmail.com>
2021-03-18 17:25:32 -0700

    starting mod 2

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,19 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if (s1.length() > s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if(s.contains("underscores")) {
+			
+		}
+		return s;
 	}
 
 	
